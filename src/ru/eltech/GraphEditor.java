1fdb2fd0505e4a4598da03e248298433f3fb603f
caxapexac <caxapexac@gmail.com>
2020-07-02 22:23:53 +0300

    Added specs

---
+++ src/ru/eltech/GraphEditor.java
@@ -0,0 +1,21 @@
+package ru.eltech;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class GraphEditor extends JPanel {
+
+    public GraphEditor() {
+        //InputStream;
+        //OutputStream
+        
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("Hello World", 30 ,30);
+    }
+
+}
---
+++ src/ru/eltech/Main.java
@@ -0,0 +1,12 @@
+package ru.eltech;
+
+import java.io.IOException;
+import javax.swing.JFrame;
+import ru.eltech.MainWindow;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+    }
+}
