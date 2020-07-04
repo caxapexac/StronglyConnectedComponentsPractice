194ceba0630b1e4783febd651454550b82e5acda
Anya <unknownladyspb@gmail.com>
2020-07-05 00:14:27 +0300

    GraphClass

---
+++ src/ru/eltech/Edge.java
@@ -0,0 +1,23 @@
+package ru.eltech;
+
+/**
+ ** @author Samoilova Anna
+ */
+
+public class Edge {
+    private Node source;
+    private Node target;
+
+    public Edge(Node source, Node target){
+        this.source = source;
+        this.target = target;
+    }
+
+    public Node getSource(){
+        return source;
+    }
+
+    public Node getTarget(){
+        return target;
+    }
+}
