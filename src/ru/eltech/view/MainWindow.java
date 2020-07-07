58639fba3dc52d1850f8caf73beae36d905207eb
Eugene Gogolev <stavrwalker@gmail.com>
2020-07-07 06:34:02 +0300

    Реализовал меню Help, пофиксил закрытие окна
    Потом вынесу html код нормально, пока пусть висит кусками в конце MainWindow

--- src/ru/eltech/view/MainWindow.java
+++ src/ru/eltech/view/MainWindow.java
@@ -1,9 +1,6 @@
 package ru.eltech.view;
 
-import ru.eltech.logic.Algorithm;
-import ru.eltech.logic.Graph;
-import ru.eltech.logic.GraphPlayer;
-import ru.eltech.logic.KosarajuAlgorithm;
+import ru.eltech.logic.*;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -45,6 +42,7 @@ public class MainWindow extends JFrame {
             @Override
             public void windowClosed(WindowEvent event) {
                 //graphPanel.serializeGraph(AUTOSAVE_FILE); TODO save
+                System.exit(0);
             }
 
             @Override
@@ -96,7 +94,7 @@ public class MainWindow extends JFrame {
         int chosenOption = fc.showOpenDialog(this);
         if (chosenOption == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fc.getSelectedFile();
-            try (FileInputStream fis = new FileInputStream(selectedFile)){
+            try (FileInputStream fis = new FileInputStream(selectedFile)) {
                 graphEditor.setRenderGraph(graph.load(fis));
                 JOptionPane.showMessageDialog(null, "Граф загружен успешно " + selectedFile.getAbsolutePath());
                 repaint();
@@ -119,14 +117,77 @@ public class MainWindow extends JFrame {
 
     public void startAlgorithm() {
         // TODO
+        //FrameList frames = KosarajuAlgorithm( graphEditor.getGraph() );
+        //graphPlayer.play(frames);
     }
 
     public void showInstruction() {
         // TODO
+        /*StringBuilder helpMsg = new StringBuilder();
+        try {
+            String path = this.getClass().getClassLoader().getResource("resorces/helpMessage.html").getPath();
+            File indexFile = new File(path);
+            Scanner helpMessageReader = new Scanner(indexFile);
+            while (helpMessageReader.hasNextLine()) {
+                helpMsg.append(helpMessageReader.nextLine());
+            }
+            helpMessageReader.close();
+            JOptionPane.showMessageDialog(this, helpMsg.toString(), "text", JOptionPane.INFORMATION_MESSAGE);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(this, "File not found", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (RuntimeException runtimeException) {
+            JOptionPane.showMessageDialog(this, runtimeException.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }*/
+        String formattedText = "<html>\n" +
+                "<font size=\"5\">\n" +
+                "    <table border=\"2\", cellspacing=\"1\", title=\"Справка\", bgcolor=\"EEEEFF\">\n" +
+                "        <caption><b><font size=\"6\", face=\"cursive, fantasy\">Доступные опции</font></b></caption>\n" +
+                "        <tr>\n" +
+                "            <th>Действие</th>\n" +
+                "            <th>Клавиша</th>\n" +
+                "        </tr>\n" +
+                "   <tr><td>Создать вершину</td><td>ЛКМ двойной клик</td></tr>\n" +
+                "   <tr><td>Открыть контекстное меню</td><td>ПКМ</td></tr>\n" +
+                "   <tr><td>Выделить вершину</td><td>ЛКМ по вершине</td></tr>\n" +
+                "   <tr><td>Переместить вершину</td><td>ЛКМ зажать</td></tr>\n" +
+                "   <tr><td>Провести ребро от выделенной вершины</td><td>удерживать ПКМ</td></tr>\n" +
+                "   <tr><td>Автовыполнение алгоритма</td><td>D</td></tr>\n" +
+                "   <tr><td>Запустить алгоритм</td><td>S</td></tr>\n" +
+                "   <tr><td>Пауза</td><td>P</td></tr>\n" +
+                "   <tr><td>Остановить алгоритм</td><td>A</td></tr>\n" +
+                "   <tr><td>Шаг вперед</td><td>D</td></tr>\n" +
+                "   <tr><td>Шаг назад</td><td>D</td></tr>\n" +
+                "   <tr><td>Удалить выделенные вершины</td><td>Delete</td></tr>\n" +
+                "   </table>\n" +
+                "</html>";
+        JLabel message = new JLabel(formattedText);
+        JOptionPane.showMessageDialog(this, message, "Help", JOptionPane.PLAIN_MESSAGE);
+
     }
 
     public void showAuthorsInfo() {
         // TODO
+        String formattedText = "<html>\n" +
+                "<font size=\"5\">\n" +
+                "    <table border=\"2\", cellspacing=\"1\", title=\"Справка\", bgcolor=\"EEEEFF\">\n" +
+                "        <caption><b><font size=\"6\", face=\"cursive, fantasy\">Авторы программы</font></b></caption>\n" +
+                "        <tr>\n" +
+                "            <th>Имя</th>\n" +
+                "            <th>Группа</th>\n" +
+                "            <th>Отвечает за</th>\n" +
+                "        </tr>\n" +
+                "        <tr><td>Сахаров Виктор</td><td>8381</td><td>графический интерфейс</td></tr>\n" +
+                "        <tr><td>Самойлова Анна</td><td>8303</td><td>логика алгоритма</td></tr>\n" +
+                "        <tr><td>Гоголев Евгений</td><td>8381</td><td>логика визуализации</td></tr>\n" +
+                "    </table>\n" +
+                "    <br>\n" +
+                "    <center>\n" +
+                "        СПбГЭТУ \"ЛЭТИ\" <br>\n" +
+                "        2020 год\n" +
+                "    </center>\n" +
+                "</font>";
+        JLabel message = new JLabel(formattedText);
+        JOptionPane.showMessageDialog(this, message, "Authors", JOptionPane.PLAIN_MESSAGE);
     }
 
     //endregion
